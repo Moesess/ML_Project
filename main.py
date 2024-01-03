d17e147fd94e4946a3fd2d78a13c6575bd185a3f
Tomasz Kaszuba <tomek@kaszuba.eu>
2024-01-03 20:58:46 +0100

    Próby przetworzenia danych - moze byc potrzeba dodac kolejne cechy z innych tabel

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import mysql.connector
 import pandas as pd
 import json
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
 def get_csv():
     db_config = {
@@ -28,5 +30,51 @@ def get_csv():
         if conn.is_connected():
             conn.close()    
 
+def learn():
+    # Wczytanie danych
+    data = pd.read_csv('temperatures.csv')
+
+    # Wstępne przetwarzanie danych
+    # Czyszczenie danych
+    data.dropna(subset=['local_temperature'], inplace=True)  # Usuwanie wierszy z brakującą temperaturą
+
+    # Przekształcanie cech kategorycznych
+    data = pd.get_dummies(data, columns=['system_mode', 'running_state'])
+    data = data[data['friendly_name'] == 'Regulator temperatury - tomek local temperature calibration']
+    data = data.drop(columns=['away_preset_days', 'comfort_temperature', 'auto_lock', 'away_mode', 'child_lock', 
+                                  'force', 'holidays', 'holidays_schedule', 'preset', 'week', 
+                                  'window_detection', 'workdays', 'workdays_schedule', 'unit_of_measurement', 
+                                  'device_class', 'icon', 'friendly_name', 'update.installed_version', 'update.state'])
+
+    # Podział danych
+    from sklearn.model_selection import train_test_split
+    train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
+
+    # Zapis przetworzonych danych
+    train_data.to_csv('train_data.csv', index=False)
+    test_data.to_csv('test_data.csv', index=False)
+
+    features = data[['local_temperature', 'current_heating_setpoint']]
+    target = data['comfort_temperature']
+
+    # Podział na zestawy treningowe i testowe
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+
+
+    # Budowa modelu regresji
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    # Ocena modelu
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    print(f'Błąd średniokwadratowy (MSE): {mse}')
+
+    # Przewidywanie temperatury komfortu
+    comfort_prediction = model.predict(X_test)
+    print(comfort_prediction)
+
+
 if __name__ == '__main__':
-    get_csv()
+    learn()
+    # get_csv()
