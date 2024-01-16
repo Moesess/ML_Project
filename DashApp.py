eeff7192c83c172fc21c437af1faee7b2f17af21
Tomasz Kaszuba <tomek@kaszuba.eu>
2024-01-16 20:51:38 +0100

    Poprawki optymalizacyjne w wyświetlaniu wykresów

--- DashApp.py
+++ DashApp.py
@@ -1,66 +1,17 @@
 import dash
 from dash import dcc
 from dash import html
+import numpy as np
 import pandas as pd
 import plotly.graph_objs as go
 from dash.dependencies import Input, Output
-from sklearn.model_selection import train_test_split
 import dash_bootstrap_components as dbc
-from LogisticReggression import LogisticRegression
 from dash_bootstrap_templates import load_figure_template
 
 
-filePath = 'CSV/Zeszyt1.xlsx'
-nazwy_kolumn = ['data','dzień tygodnia','godzina','zużycie energii']
-xls = pd.ExcelFile(filePath)
-dane_wejsciowe_X = xls.parse('styczeń', usecols=nazwy_kolumn)
+filePath = 'CSV/power_predict.csv'
 
-#Przekształcamy pola w macierzy na odpowiednie type
-dane_wejsciowe_X['data'] = pd.to_datetime(dane_wejsciowe_X['data'], format='%Y-%m-%d')
-# Przekształć kolumnę 'godzina' na format string
-dane_wejsciowe_X['godzina_str'] = dane_wejsciowe_X['godzina'].astype(str)
-
-
-# Przekształć kolumnę 'data' na cechy liczbowe
-dane_wejsciowe_X['rok'] = dane_wejsciowe_X['data'].dt.year
-dane_wejsciowe_X['miesiac'] = dane_wejsciowe_X['data'].dt.month
-dane_wejsciowe_X['dzien'] = dane_wejsciowe_X['data'].dt.day
-dane_wejsciowe_X['godzina'] = dane_wejsciowe_X['godzina_str'].str.split(':').str[0].astype(int)
-dane_wejsciowe_X['minuta'] = dane_wejsciowe_X['godzina_str'].str.split(':').str[1].astype(int)
-
-# Usuń kolumnę 'data', ponieważ już ją przekształciliśmy
-dane_wejsciowe_X.drop(['data','godzina_str'], axis=1, inplace=True)
-
-#dane_wejsciowe_X = pd.read_excel(filePath, sheet_name = ['styczeń'], usecols = nazwy_kolumn)
-
-#robimy to samo dla danych wejściowych Y
-xls2 = pd.ExcelFile(filePath)
-dane_wejsciowe_Y =xls.parse('styczeń', usecols = ['obecnosc'])
-
-X, y = dane_wejsciowe_X, dane_wejsciowe_Y
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size = 0.2, random_state = 1234)
-
-classifier = LogisticRegression(learning_rate=0.00001)
-y_train = y_train.squeeze().values
-classifier.fit(X_train, y_train)
-
-X_chart = X
-Y_chart = y
-
-results = classifier.predict(X_chart)
-
-
-X_chart["obecność"] = results
-X_chart['data'] = pd.to_datetime(X_chart['rok'].astype(str) + '-' + 
-                                 X_chart['miesiac'].astype(str) + '-' + 
-                                 X_chart['dzien'].astype(str) + ' ' + 
-                                 X_chart['godzina'].astype(str) + ':' + 
-                                 X_chart['minuta'].astype(str))
-X_chart.sort_values(by="data", inplace=True)
-
-X_chart_filtered = X_chart[
-    (X_chart['data'].dt.date >= pd.to_datetime('2023-01-01').date()) & 
-    (X_chart['data'].dt.date <= pd.to_datetime('2023-01-07').date())]
+dane_wejsciowe = pd.read_csv(filePath)
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 template = "DARKLY"
@@ -73,18 +24,17 @@ app.layout = html.Div(children=[
         html.Label('Wybierz datę początkową:'),
         dcc.DatePickerSingle(
             id='date-picker-start',
-            date='2023-01-01'
+            date='2023-02-01'
         ),
         html.Label('Wybierz datę końcową:'),
         dcc.DatePickerSingle(
             id='date-picker-end',
-            date='2023-01-01'
+            date='2023-02-01'
         )
     ]),
 
     dcc.Graph(
         id='graph',
-        # figure=fig
     )
 ])
 
@@ -96,35 +46,38 @@ app.layout = html.Div(children=[
 )
 def update_graph(start_date, end_date):
     # Filtrowanie danych na podstawie wybranego zakresu dat    
-    filtered_data = X_chart[(X_chart['data'].dt.date >= pd.to_datetime(start_date).date()) &
-                            (X_chart['data'].dt.date <= pd.to_datetime(end_date).date())]
+    filtered_data = dane_wejsciowe[(pd.to_datetime(dane_wejsciowe['date']).dt.date >= pd.to_datetime(start_date).date()) &
+                                   (pd.to_datetime(dane_wejsciowe['date']).dt.date <= pd.to_datetime(end_date).date())]
         
-    # Tworzenie wykresu
-    traces = []
+    # Tworzenie danych dla wykresu obecności
+    presence_data = filtered_data[filtered_data['presence'] == 1]
+    presence_data['date'] = pd.to_datetime(presence_data['date'])
+    max_power = filtered_data['power'].max() + 10
+
+    # Współrzędne X i Y dla linii pionowych
     obecnosc_x = []
     obecnosc_y = []
-    for i, row in filtered_data.iterrows():
-        if row['obecność'] == 1:
-            obecnosc_x.extend([row['data'], row['data'] + pd.Timedelta(minutes=1), None])
-            obecnosc_y.extend([0, max(filtered_data['zużycie energii']), None])
-    
-    traces.append(
+    for ts in presence_data['date']:
+        obecnosc_x.extend([ts, ts, None])
+        obecnosc_y.extend([0, max_power, None])
+
+    traces = [
         go.Scatter(
             x=obecnosc_x,
             y=obecnosc_y,
             mode='lines',
             name='Obecność',
+            line=dict(color='pink', width=2),
             opacity=0.3
-        )
-    )
-    traces.append(
+        ),
         go.Scatter(
-            x=filtered_data['data'],
-            y=filtered_data['zużycie energii'],
+            x=filtered_data['date'],
+            y=filtered_data['power'],
             mode='lines', 
-            name='Zużycie energii',
-        )
-    )
+            line=dict(color='yellow', width=2),
+            name='Zużycie energii'
+        ),
+    ]
 
     # Layout wykresu z dodanymi przedziałami obecności
     layout = go.Layout(
